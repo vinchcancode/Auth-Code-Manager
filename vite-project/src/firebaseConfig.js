0e2081ffcb2f4c7095419baceffcce549edafd47
rafael vincent <smurf4812@gmail.com>
2025-03-16 11:08:29 +0800

    completed for testing

--- vite-project/src/firebaseConfig.js
+++ vite-project/src/firebaseConfig.js
@@ -1,16 +1,24 @@
-// src/firebaseConfig.js
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore, collection, getDocs, addDoc, doc } from "firebase/firestore";
 
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAVuxRQfoRHfgwxn4nPHpjiTH4_MMLA4Fs",
   authDomain: "auth-code-manager.firebaseapp.com",
   projectId: "auth-code-manager",
-  storageBucket: "auth-code-manager.appspot.com", // ✅ Fixed storageBucket
+  storageBucket: "auth-code-manager.firebasestorage.app",
   messagingSenderId: "58509409186",
   appId: "1:58509409186:web:86507c489f69f01defd0fb",
-  measurementId: "G-KRM8TXBP1Z",
+  measurementId: "G-KRM8TXBP1Z"
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db, collection, getDocs, addDoc, doc };
+
